c65176adac7440da184726f18c8985c73e179d08
Mikael Stockman <mikael.stockman@slagkryssaren.com>
2018-09-26 00:11:31 +0900

    Webapp that can receive data and presumably broadcast it to all listening clients.

--- app.js
+++ app.js
@@ -1,15 +1,76 @@
 const os = require('os');
 const http = require('http');
+const webSocketServer = require('websocket').server;
 
 const port = 3000;
 const hostname = os.hostname();
-
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
   });
-
 server.listen(port, hostname, () => {
     console.log('Server running at http://127.0.0.1:' + port)
+});
+
+
+/*
+ * Websocket stuff 
+ */
+var history = [ ];
+var clients = [ ];
+var publisher = null;
+
+const wsServer = new webSocketServer({httpServer: server});
+
+wsServer.on('request', function(request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+    if (request.resource !== '/publisher' 
+        && request.resource !== '/subscriber') {
+            console.log("Invalid resource: " + request.resource );
+            request.reject();
+            return;
+    }
+
+    const connection = request.accept(null, request.origin); 
+    console.log((new Date()) + ' Connection accepted.');
+
+    if (request.resource === '/publisher') {
+        publisher = connection;
+        connection.on('message', function(message) {
+            if (message.type === 'utf8') {
+                jsonData = JSON.parse(message.utf8Data);
+            
+                if (jsonData.type === 'post_data') {
+                    console.log("Received data: " + message.utf8Data)
+                    history.push(jsonData)
+                    history = history.slice(-100);
+                    
+                    for (var i=0; i < clients.length; i++) {
+                        clients[i].sendUTF(message.utf8Data);
+                    }
+                } else {
+                    console.log("Received invalid payload type: '" + jsonData.type +"'")
+                }
+            }
+        });
+        // publisher disconnected
+        connection.on('close', function(connection) {
+            console.log((new Date()) + " Publisher " + connection.remoteAddress + " disconnected.");
+            publisher = null;
+        });
+    } else if (request.resource === '/subscriber') {
+        const index = clients.push(connection) - 1;
+        // send back chat history
+        if (history.length > 0) {
+            connection.sendUTF(
+                JSON.stringify({ type: 'history', data: history} )
+            );
+        }
+        // subscriber disconnected
+        connection.on('close', function(connection) {
+            console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+            clients.splice(index, 1);
+        });
+    }
 });
