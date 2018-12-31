5e0e6addb9f338652d9096a0c20d70827ef1fb7b
Mikael Stockman <mikael.stockman@slagkryssaren.com>
2018-12-31 16:48:48 +0900

    Support for new ADC and bugfixes

--- seism_logger.py
+++ seism_logger.py
@@ -19,8 +19,28 @@ CLK  = 18
 MISO = 23
 MOSI = 24
 CS   = 25
-mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
+class MCP3208(Adafruit_MCP3008.MCP3008):
+    def read_adc(self, adc_number):
+        """Read the current value of the specified ADC channel (0-7).  The values
+        can range from 0 to 1023 (10-bits).
+        """
+        assert 0 <= adc_number <= 7, 'ADC number must be a value of 0-7!'
+        # Build a single channel read command.
+        # For example channel zero = 0b11000000
+        command = 0b11 << 6                  # Start bit, single channel read
+        command |= (adc_number & 0x07) << 3  # Channel number (in 3 bits)
+        # Note the bottom 3 bits of command are 0, this is to account for the
+        # extra clock to do the conversion, and the low null bit returned at
+        # the start of the response.
+        resp = self._spi.transfer([command, 0x0, 0x0])
+        # Parse out the 12 bits of response data and return it.
+        result = (resp[0] & 0x01) << 11  # only B11 is here
+        result |= resp[1] << 3           # B10:B3
+        result |= resp[2] >> 5           # MSB has B2:B0 ... need to move down to LSB
+        return (result & 0x0FFF)  # ensure we are only sending 12b
+
+mcp = MCP3208(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 current_millis = lambda: int(round(time.time() * 1000))
 
 def on_message(ws, message):
@@ -36,21 +56,21 @@ def on_open(ws):
     print("### opened ###")
     def run(*args):
         sample_avg_count = 6
-        sample_rate = 60
+        sample_rate = 90
         sleep_time = 1/sample_rate
-        buffer_size = 20
+        # 2 seconds worth of data
+        buffer_size = int(sample_rate/sample_avg_count) * 2
         buffer = [0] * buffer_size
 
         while True:
-            
             for i in range(0, buffer_size):
                 tmp_value_sum = 0
                 for j in range(0, sample_avg_count):
-                    tmp_value_sum += mcp.read_adc(0)
+                    tmp_value_sum += (mcp.read_adc(2) - 2000)
                     if j < sample_avg_count-1:
                         time.sleep(sleep_time)
                 avg_value = tmp_value_sum / sample_avg_count
-                buffer[i] = avg_value
+                buffer[i] = round(avg_value*8)
 
             t1 = datetime.datetime.now()
             data = '{"values": ' + json.dumps(buffer) + ', "type": "post_data"}'
@@ -78,4 +98,3 @@ if __name__ == "__main__":
             time.sleep(5)
         except Exception as e:
             print(e)
-
